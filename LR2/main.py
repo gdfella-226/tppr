99a4dbcb28bb6e1cb01467b9923ff1b2104d6efc
gdfella-226 <kosorev.daniel@gmail.com>
2024-11-25 10:55:59 +0300

    update

--- LR2/main.py
+++ LR2/main.py
@@ -1,3 +1,4 @@
+import scipy.stats
 import numpy as np
 from itertools import permutations
 
@@ -32,8 +33,15 @@ def spearman_corr(x):
             for k in range(n):
                 _sum += (x[k, i] - x[k, j])**2
             corr = 1 - (6 * _sum)/(n**3 - n)
-            correlations[i, j] = corr
-            correlations[j, i] = corr
+            
+            df = n - 2
+            t_stat = corr * ((n - 2) ** 0.5) / ((1 - corr**2) ** 0.5)
+            if 2 * scipy.stats.t.sf(abs(t_stat), df) < 1 - 0.95:
+                correlations[i, j] = corr
+                correlations[j, i] = corr
+            else:
+                correlations[i, j] = None
+                correlations[j, i] = None
 
     return correlations
 
@@ -55,6 +63,14 @@ def generalize(x, exp_c):
 
     return np.argsort(weighted_rankings) + 1, best_rank
 
+def gaz(mtx):
+    k = expert_comp(mtx)
+    kc = kendall_corr(mtx)
+    sc = spearman_corr(mtx)
+    g = generalize(mtx, k)
+    return k, kc, sc, g
+
+
 if __name__ == '__main__':
     x = np.array([
         [1, 8, 1, 1],
@@ -67,10 +83,18 @@ if __name__ == '__main__':
         [8, 1, 3, 2]
     ])
 
-    k = expert_comp(x)
-    kc = kendall_corr(x)
-    sc = spearman_corr(x)
-    g = generalize(x, k)
+    y = np.array([
+        [7, 8, 7, 7],
+        [8, 7, 8, 8],
+        [6, 5, 6, 6],
+        [5, 6, 5, 5],
+        [3, 3, 3, 3],
+        [2, 2, 2, 2],
+        [1, 4, 4, 4],
+        [4, 1, 1, 1]
+    ])
+
+    k, kc, sc, g = gaz(x) 
     
     print(f'Коэффициенты компетентности экспертов: {k}')
     print(f'Средняя выборка: {g[0]}')
