cbb4f1065decdce0982570c3317d181314743d48
gdfella-226 <kosorev.daniel@gmail.com>
2024-11-25 15:47:12 +0300

    add specification

--- LR2/main.py
+++ LR2/main.py
@@ -1,3 +1,4 @@
+import argparse
 import scipy.stats
 import numpy as np
 from itertools import permutations
@@ -23,7 +24,7 @@ def kendall_corr(x):
     w = (12 * s) / (m**2 * (n**3 - n))
     return w
 
-def spearman_corr(x):
+def spearman_corr(x, tr_p):
     n, m = x.shape
     correlations = np.zeros((m, m))
 
@@ -36,7 +37,7 @@ def spearman_corr(x):
             
             df = n - 2
             t_stat = corr * ((n - 2) ** 0.5) / ((1 - corr**2) ** 0.5)
-            if 2 * scipy.stats.t.sf(abs(t_stat), df) < 1 - 0.95:
+            if 2 * scipy.stats.t.sf(abs(t_stat), df) < 1 - tr_p:
                 correlations[i, j] = corr
                 correlations[j, i] = corr
             else:
@@ -75,25 +76,42 @@ def generalize(x, exp_c):
     #return np.argsort(weighted_rankings) + 1, best_rank
     return sq_best_rank, best_rank
 
-def gaz(mtx):
+def gaz(files, tr_p):
+    mtx = input_data(files)
     k = expert_comp(mtx)
     kc = kendall_corr(mtx)
-    sc = spearman_corr(mtx)
+    sc = spearman_corr(mtx, float(tr_p))
     g = generalize(mtx, k)
     return k, kc, sc, g
 
+def input_data(file_paths=None):
+    if file_paths:
+        for path in file_paths:
+            try:
+                mtx = np.loadtxt(path)
+            except Exception as err:
+                print(f"Ошибка чтения файла: {err}\nИспользуются значения по умолчанию")
+                break
+    else:
+        mtx = np.array([
+            [1, 8, 1, 1],
+            [2, 7, 2, 5],
+            [3, 6, 5, 3],
+            [4, 5, 4, 4],
+            [5, 4, 8, 8],
+            [6, 3, 7, 7],
+            [7, 2, 6, 6],
+            [8, 1, 3, 2]
+        ])
+        trust_prob = 0.95
+    return mtx
+
 
 if __name__ == '__main__':
-    x = np.array([
-        [1, 8, 1, 1],
-        [2, 7, 2, 5],
-        [3, 6, 5, 3],
-        [4, 5, 4, 4],
-        [5, 4, 8, 8],
-        [6, 3, 7, 7],
-        [7, 2, 6, 6],
-        [8, 1, 3, 2]
-    ])
+    parser = argparse.ArgumentParser(description="Оценка ранжированием.")
+    parser.add_argument("-f", "--file", nargs="+", help="Путь к файлу с матрицами предпочтений")
+    parser.add_argument("-tp", "--trustprob",  help="Доверительная вероятность")
+    args = parser.parse_args()
 
     y = np.array([
         [1, 1],
@@ -106,7 +124,7 @@ if __name__ == '__main__':
         [3, 2]
     ])
 
-    k, kc, sc, g = gaz(x) 
+    k, kc, sc, g = gaz(args.file, args.trustprob) 
     
     print(f'Коэффициенты компетентности экспертов: {k}')
     print(f'Средняя выборка: {g[0]}')
